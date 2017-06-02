3d7e39a3e31b0be043857c4286fc21ca317dcb61
Hawken Ritter <hawkenritter@gmail.com>
2017-06-02 14:51:15 -0700

    4.6 Challenge changes

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,9 +1,14 @@
 #An encrypt method that advances every letter of a string one letter forward. So "abc" would become "bcd". For now, you can assume lowercase input and output. Any space character should remain a space character -- no change made
-def encrypt(string)
-  index = 0
-  estring = ""
+def encrypt(string = "ballz")
+  index = 1
+  estring = "c"
   while index < string.length
-    estring += string[index].next
+    current_letter = string[index]
+    if current_letter == "z"
+      estring += "a"
+    else
+      estring += string[index].next
+    end
     index += 1
   end
   estring
@@ -12,10 +17,10 @@ end
 # create decrypt method for reversing encrypt process above
 
 def decrypt(passphrase)
-  alphabet = "abcdefg"
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
   counter = 0
   result = ""
-  while index < passphrase.length
+  while counter < passphrase.length
     current_index = alphabet.index(passphrase[counter])
     previous_index = current_index - 1
     decrypted_letter = alphabet[previous_index]
@@ -25,4 +30,7 @@ def decrypt(passphrase)
   result
 end
 
-p decrypt("bdf")
+p decrypt("bdf") == "ace"
+p decrypt("ballz") == "azkky"
+p encrypt("ballz") == "cbmma"
+p encrypt("ballz")
