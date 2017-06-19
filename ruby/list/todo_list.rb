369e88bdea39ed6a150a337f132df1c1452b0ac0
Hawken Ritter <hawkenritter@gmail.com>
2017-06-19 15:25:09 -0700

    Go green, pair rspec tests

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,16 +1,22 @@
 class TodoList
-  def initialize(dishes, lawn)
-    @dishes = dishes
-    @lawn = lawn
+  def initialize(items)
+    @items = items
   end
   def get_items
-    list_array = []
-      list_array << @dishes
-      list_array << @lawn
-    p list_array
+    @items
   end
-
+  def add_item(new_item)
+    @items << new_item
+  end
+  def delete_item(old_item)
+    @items.delete(old_item)
+  end
+  def get_item(position)
+    index_position = position.to_i
+    @items[index_position]
+  end
 end
 
-#list = TodoList.new("Do the dishes", "Mow the lawn")
-#p list.get_items
+#list = TodoList.new(["do the dishes", "mow the lawn"])
+#p list.get_items(0)
+
