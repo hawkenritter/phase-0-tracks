11cff2d8f798d3613fa353c29a3821cb36340393
Hawken Ritter <hawkenritter@gmail.com>
2017-06-22 18:07:32 -0700

    Reflect on gps

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -10,20 +10,23 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+  #initialize an instance of the class virus predictor, i.e. for each state
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #come back to...
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  #takes three arguments... conditional loop predicting deaths based on variables of density and population from each state.. predicts death count... prints result
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -41,7 +44,8 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #takes two arguments, speed is adjusted based on population density, prints result.
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -65,23 +69,43 @@ class VirusPredictor
 end
 
 #=======================================================================
+#Alaska will lose 36572 people in this outbreak and will spread across the state in 2.5 months.
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+# iterate through the states,
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+STATE_DATA.each do |state_origin, population_data|
+  state_origin = VirusPredictor.new(state_origin, population_data[:population_density],population_data[:population])
+  #p state_origin
+  #p population_data
+  state_origin.virus_effects
+end
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+#alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects#
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+##jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+##jersey.virus_effects##
+
+##california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+##california.virus_effects##
+
+#alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+#alaska.virus_effects
 
 
 #=======================================================================
 # Reflection Section
+
+#there are hash rockets and hash symbols. Hash rockets are used like this: "Key name" => "Value", and hash symbols are used like this: key_name: "Value"... hash symbols are more common for engineers just because they are easier to type, but if you plan on printing your keys it makes more sense to use hash rockets because they print strings vs symbols.
+
+#require_relative links files, giving one file access to the data in another in the relative directory.
+
+#There are multiple ways to iterate through a hash such as using the multiple argument approach in a do loop.
+
+#What stood out about virus_effects was the fact that we were passing the arguments of our instance variables even though they were available to the entire class. Thus we could delete these arguments and just call them within the method because they were available as instance variables.
+
+#I think I solidified my understanding of the scope of instance variables and how they work inside of a class. I was unnecessarily using attr:reader/writer because I thought I needed to in order for instance methods to access the variables...
