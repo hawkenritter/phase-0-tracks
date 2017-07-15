7bc9a5dd94c956bd558d981b721ac4e0471885fb
Hawken Ritter <hawkenritter@gmail.com>
2017-07-15 10:42:03 -0700

    9.3 exercise

---
+++ web_dev/jquery/js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+  alert("hello?");
+});
+
+$(functionCss() {
+$('p').on('click', functionCss(){
+  $(this).css('border','4px solid red');}
+});
+$(toggleFunction() {
+$('p').toggle(function() {
+  $('p').css()
+}, function() {
+
+})
+});
